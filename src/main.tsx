cfc66db277282ce775b1812408b6b98d224fc6ad
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-11-17 11:12:32 +0200

    chore: config setup - ts, eslint, prettier, mantine, vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MantineProvider, createTheme } from '@mantine/core';
+import { App } from './App';
 
+const theme = createTheme({
+  /** theme overrides here */
+});
+
+// TODO: no strict mode for production
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  base: './',
+  build: {
+    outDir: 'build',
+  },
+});
